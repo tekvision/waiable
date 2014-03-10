d20e93975653aa1cdae568a594c7f2a505c20edd
Nitin Dhaware <nitin@techvision.net.in>
2014-03-10 16:39:51 +0530

    Added the test file with gem name. Have written test cases for link_to and table_for

---
+++ test/test_waiable.rb
@@ -0,0 +1,24 @@
+require 'minitest/autorun'
+require './lib/waiable'
+
+class TestWaiable < Minitest::Test
+  include Waiable
+  def test_link_to_with_text
+    assert_equal('<a href="http://www.example.com">Hello</a>', link_to("Hello", "http://www.example.com"))
+  end
+
+  def test_link_to_with_image
+    assert_equal('<a href="http://www.example.com"><img src="image.jpg" alt="This is an image"></a>', link_to(image_tag("image.jpg", {:alt => "This is an image"}), "http://www.example.com"))
+  end
+
+  def test_table_for_with_summary
+    assert_equal('<table summary="This is table summary"></table>', table_for({summary: "This is table summary"))
+  end
+
+  def test_table_for_with_headers
+
+  end
+
+
+
+end
