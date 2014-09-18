a008731e2728ecea1187032b170a5faf1878da88
Siddhant Chothe <sidnc86@gmail.com>
2014-09-18 08:40:53 +0530

    Corrected syntactical errors to integration test.

--- test/integration/test_helper.rb
+++ test/integration/test_helper.rb
@@ -3,11 +3,11 @@ require './lib/waiable/helpers/helper'
 
  class HelperTest < ActionDispatch::IntegrationTest
 
-  test "should be able render a grid element with id attribute" do
+  test "should render a grid element with id attribute" do
     assert_equal('<div id = "grid1"></div>', grid_for("grid1"))
   end
 
-  test "should be able to generate a grid element with rows and cells" do
+  test "should generate a grid element with rows and cells" do
     xml = XmlSimple.xml_in(%%
       <div id="grid1" role="grid" tabindex="-1" aria-selected="false">
         <div id="grid1_row_0" class="row" role="row">
@@ -33,10 +33,10 @@ require './lib/waiable/helpers/helper'
 	  <div role="gridcell"></div>
 	</div>
       </div>%)  
-    assert_equal(XmlSimple.xml_in(NormaliseSpace' => 2), xml))
+    assert_equal(XmlSimple.xml_in('NormaliseSpace' => 2, xml), grid_for(:grid1, rows => 2, columns => 2))
   end
 
-  test "should be render a grid element with data" do
+  test "should render a grid element with data" do
     xml = XmlSimple.xml_in(%%
       <div id="grid1" role="grid" tabindex="-1" aria-selected="false">
         <div id="grid1_row_0" class="row" role="row">
@@ -56,6 +56,6 @@ require './lib/waiable/helpers/helper'
 	  </div>
 	</div>
       </div>%)
-    assert_equal(XmlSimple.xml_in('NormaliseSpace' => 2), xml, grid_for(grid1, data["Grid cell 1", "Grid cell 2", "Grid cell 3", "Grid cell 4"]))
+    assert_equal(XmlSimple.xml_in('NormaliseSpace' => 2), xml), grid_for(:grid1, data => [["Grid cell 1", "Grid cell 2"], ["Grid cell 3", "Grid cell 4"]]))
   end
 end
