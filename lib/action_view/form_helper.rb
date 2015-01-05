0d00c1e0a5804c2cdc90165f122c455253e26ed7
sanghapal <sanghapalbhowate89@gmail.com>
2015-01-05 12:48:38 -0500

    Implement the aria-labelledby attribute in the text_field helper method. ooverride the render method to waiable gem.

--- lib/action_view/form_helper.rb
+++ lib/action_view/form_helper.rb
@@ -15,5 +15,20 @@ module ActionView
       end
 
     end
+
+    module Tags
+      class TextField < Base # :nodoc:
+        def render
+          options = @options.stringify_keys
+          options["size"] = options["maxlength"] unless options.key?("size")
+          options["type"] ||= field_type
+          options["value"] = options.fetch("value") { value_before_type_cast(object) } unless field_type == "file"
+          options["value"] &&= ERB::Util.html_escape(options["value"])
+          options["aria-labelledby"] = ["label_#{add_default_name_and_id(options)}"]
+          add_default_name_and_id(options)
+          tag("input", options)
+        end
+      end
+    end
   end
 end
