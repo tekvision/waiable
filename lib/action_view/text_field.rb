4d5e3d8b4e08e6ffa8565e4e05aacdef5a632c21
sanghapal <sanghapalbhowate89@gmail.com>
2015-02-05 18:58:49 -0500

    modified the add_default_name_and_id method.

--- lib/action_view/text_field.rb
+++ lib/action_view/text_field.rb
@@ -13,14 +13,10 @@ module Waiable
         end
       end
 
-#      def add_default_name_and_id(options)
-#        super
-#        options["aria-labelledby"] = "label_#{options["id"]}"
-#	if options["maxlength"]
-#	  options["aria-describedby"] = "maxlength_#{options["id"]}"
-#	end
-#options
-#      end
+      def add_default_name_and_id(options)
+        super
+        options["aria-labelledby"] = "label_#{options["id"]}"
+      end
 
       def options_for_rendering
         options = @options.stringify_keys
@@ -29,7 +25,7 @@ module Waiable
         options["value"] = options.fetch("value") {value_before_type_cast(object) } unless field_type == "file"
         #The following method can be overridden separately
         #to generate aria-describedby="maxlength_person_first_name" for the <input ... /> field
-        options["aria-labelledby"] = "label_#{options["id"]}"
+#        options["aria-labelledby"] = "label_#{options["id"]}"
 	if options["maxlength"]
 	  options["aria-describedby"] = "maxlength_#{add_default_name_and_id(options)}"
 	end
