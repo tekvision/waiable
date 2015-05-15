1240261de0640bb49f3e9a92cc6e780575af0bca
Shantanu Choudhury <choudhuryshantanu00@gmail.com>
2015-05-15 04:54:22 -0700

    Implemented common functionalities in basemethods.rb

--- lib/action_view/text_field.rb
+++ lib/action_view/text_field.rb
@@ -1,11 +1,12 @@
 require 'pry'
+require 'action_view/basemethods'
 require 'active_support/concern'
 
 module Waiable
   module Base
     module TextField
       extend ActiveSupport::Concern
-
+include CommonMethods
 
       TAG_PREFIXES = ['aria', 'data', :aria, :data].to_set
       included do
@@ -14,47 +15,9 @@ module Waiable
           aria_content(@options)
         end
       end
+      
 
       
-      def options_for_rendering
-        options = @options.stringify_keys
-        options["size"] = options["maxlength"] unless options.key?("size")
-        options["type"] ||= field_type
-        options["value"] = options.fetch("value") {value_before_type_cast(object) } unless field_type == "file"
-        options["aria"] ||= {}
-        options["aria"]["required"] = true if required_field?(@method_name)
-        #The following method can be overridden separately
-        #to generate aria-describedby="maxlength_person_first_name" for the <input ... /> field
-        @options = options
-        add_aria_describedby_values(@options)
-        add_default_name_and_id(@options)
-      end
-
-      def add_aria_describedby_values(options)
-        arr_describedby = []
-        options['aria'] ||= {}
-          if options["maxlength"]
-          arr_describedby << "maxlength_#{add_default_name_and_id(options)}"
-	  options["aria"]["describedby"] = arr_describedby.join(", ")
-	end
-        arr_describedby << "error_#{add_default_name_and_id(options)}" if object_has_errors?
-        options["aria"]["describedby"] = arr_describedby.join(", ")
-      end
-
-
-      def required_field?(field)
-        object_class = @object_name.classify.constantize
-        if object_class.respond_to?(:validators_on)
-          object_class.validators_on(field.intern).detect do |validator|
-            if validator.is_a? ActiveModel::Validations::PresenceValidator
-              validator.is_a? ActiveModel::Validations::PresenceValidator
-            elsif validator.is_a? Mongoid::Validatable::PresenceValidator
-              validator.is_a? Mongoid::Validatable::PresenceValidator
-            end
-          end.present?
-        end
-      end
-
       def aria_content(options)        
 	aria = ""        
 	if options["maxlength"]
